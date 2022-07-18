8f9064ead0066664ffccfc1ec86c5fe545074f00
Sergey Popov <popserg5555@gmail.com>
2022-07-18 20:08:11 +0300

    PSR-1,4,12; package Logger

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require 'vendor/autoload.php';
+
+use App\core\Router;
+use App\lib\Session\Session;
+
+$session = new Session();
+$session->start();
+$router = new Router();
+$router->run();
